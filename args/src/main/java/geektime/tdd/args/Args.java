507634904905d892f5938fc9ed8c65b9645b02a6
zhangtralon <ztralon@hotmail.com>
2024-06-22 09:39:57 +0800

    exercise refactor

--- args/src/main/java/geektime/tdd/args/Args.java
+++ args/src/main/java/geektime/tdd/args/Args.java
@@ -19,32 +19,30 @@ public class Args<T> {
     public static <T> T parse(Class<T> optionsClass, String... args) {
         return new Args<T>(optionsClass, PARSERS).parse(args);
     }
-
-    private Class<T> optionsClass;
-    private Map<Class<?>, OptionParser> parsers;
+    Class<T> optionsClass;
+    Map<Class<?>, OptionParser> parsers;
 
     public Args(Class<T> optionsClass, Map<Class<?>, OptionParser> parsers) {
         this.optionsClass = optionsClass;
         this.parsers = parsers;
     }
 
-    public T parse(String...args) {
+    private static Object parseOption(List<String> arguments, Parameter parameter, Map<Class<?>, OptionParser> parsers) {
+        if (!parameter.isAnnotationPresent(Option.class)) throw new IllegalOptionException(parameter.getName());
+        return parsers.get(parameter.getType()).parse(arguments, parameter.getAnnotation(Option.class));
+    }
+
+    public T parse(String... args) {
         try {
             List<String> arguments = Arrays.asList(args);
             Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
-            Object[] values = Arrays.stream(constructor.getParameters()).map(it -> parseOption(arguments, it)).toArray();
+            Object[] values = Arrays.stream(constructor.getParameters()).map(it -> parseOption(arguments, it, parsers)).toArray();
             return (T) constructor.newInstance(values);
         } catch (IllegalOptionException e) {
             throw e;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private Object parseOption(List<String> arguments, Parameter parameter) {
-        if(!parameter.isAnnotationPresent(Option.class)) throw new IllegalOptionException(parameter.getName());
-        return parsers.get(parameter.getType()).parse(arguments, parameter.getAnnotation(Option.class));
     }
 
 }
