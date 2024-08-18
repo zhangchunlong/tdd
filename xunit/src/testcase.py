cf060b0c21de51dc2dfdecc8f9d1a8553d9c6b2d
zhangtralon <ztralon@hotmail.com>
2024-08-18 15:39:46 +0800

    finish xunit

--- xunit/src/testcase.py
+++ xunit/src/testcase.py
@@ -2,10 +2,16 @@ class TestCase(object):
     def __init__(self, name):
         self.name = name
 
-    def run(self):
-        self.setUp()
-        method = getattr(self, self.name)
-        method()
+    def run(self, result):
+        result.testStarted()
+
+        try:
+            self.setUp()
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
+
         self.tearDown()
 
     def setUp(self):
@@ -15,14 +21,28 @@ class TestCase(object):
         pass
 
 
-class WasRun(TestCase):
-    def setUp(self):
-        self.wasRun = None
-        self.log = "setUp "
+class TestResult(object):
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
 
-    def testMethod(self):
-        self.wasRun = 1
-        self.log = self.log + "testMethod "
+    def testStarted(self):
+        self.runCount = self.runCount + 1
 
-    def tearDown(self):
-        self.log = self.log + "tearDown "
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
+
+    def summary(self):
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
+
+
+class TestSuite(object):
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
--- xunit/tests/testcasetest.py
+++ xunit/tests/testcasetest.py
@@ -1,19 +1,83 @@
 # -*- coding: utf-8 -*-
 
-from src.testcase import WasRun, TestCase
+from src.testcase import TestCase, TestResult, TestSuite
+
+
+class WasRun(TestCase):
+    def setUp(self):
+        self.log = "setUp "
+
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+
+    def testBrokenMethod(self):
+        raise Exception
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
+
+class FailedSetUp(TestCase):
+    def setUp(self):
+        raise Exception
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
+
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        test.run()
+        test.run(self.result)
         assert("setUp testMethod tearDown " == test.log)
 
+    def testResult(self):
+        test = WasRun("testMethod")
+        test.run(self.result)
+        assert("1 run, 0 failed" == self.result.summary())
 
-# TODO： 调用测试方法之后调用tearDown
-# TODO: 即使测试方法执行识别也会调用tearDown
-# TODO: 执行符合测试
-# TODO：报告测试结果
-# TODO: 在WasRun的日志中记录调用串
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        test.run(self.result)
+        assert("1 run, 1 failed" == self.result.summary())
 
-TestCaseTest("testTemplateMethod").run()
+    def testFailedMethodWithTeardown(self):
+        test = WasRun("testBrokenMethod")
+        test.run(self.result)
+        assert("setUp tearDown " == test.log)
+
+    def testFailedResultFormatting(self):
+        self.result.testStarted()
+        self.result.testFailed()
+        assert("1 run, 1 failed" == self.result.summary())
+
+    def testFailedSetup(self):
+        test = FailedSetUp("testMethod")
+        test.run(self.result)
+
+        assert ("1 run, 1 failed" == self.result.summary())
+
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
+
+# TODO: 从TestCase类中创建TestSuite对象
+
+
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testSuite"))
+suite.add(TestCaseTest("testFailedMethodWithTeardown"))
+suite.add(TestCaseTest("testFailedSetup"))
+result = TestResult()
+suite.run(result)
+
+print(result.summary())
+
+
